f875df8c2407594ceaecc292a65d00b1746f20a5
alexhahahaDrag <734663446@qq.com>
2023-01-02 14:29:27 +0800

    修改前端项目打包后不能访问问题2

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ import FinanceManager from '@v/finance/financeManager/index.vue';
 import FinanceAnalysis from '@v/finance/financeAnalysis/index.vue';
 import UserManager from '@v/user/userManager/index.vue';
 import NProgress from 'nprogress';
-// import { useUserStore } from "@/store/modules/user/user";
+import { useUserStore } from "@/store/modules/user/user";
 
 export const routes: MenuDataItem[] = [
   {
@@ -146,23 +146,18 @@ export const routes: MenuDataItem[] = [
     routes,
   });
   
-  router.beforeEach(
-      () => {
-      // (to, from, next)  => {
-    // const userStore = useUserStore();
-    // console.log(to);
-    // console.log(from);
-    // console.log(next);
+  router.beforeEach((to, from, next)  => {
+    const userStore = useUserStore();
     NProgress.start() // start progress bar
-    // if (userStore.getToken) {  // 判断当前的token是否存在
-    //   console.log(from);
-    //   next();
-    // } else {
-    //   next({
-    //     path: '/login',
-    //     query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
-    //   })
-    // }
+    if (userStore.getToken) {  // 判断当前的token是否存在
+      next();
+      console.log('from' + from)
+    } else {
+      next({
+        path: '/login',
+        query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+      })
+    }
     return true
   });
 
