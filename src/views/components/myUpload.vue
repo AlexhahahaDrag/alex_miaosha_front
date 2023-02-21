cef665275e5104996b73f090dff9eff56c0b83af
alexhahahaDrag <734663446@qq.com>
2023-02-21 19:38:43 +0800

    更新

--- src/views/components/myUpload.vue
+++ src/views/components/myUpload.vue
@@ -1,7 +1,17 @@
 <template>
-    <a-upload v-model:file-list="fileList" name="avatar" list-type="picture-card" class="avatar-uploader"
+    <!-- <a-upload v-model:file-list="fileList" name="avatar" list-type="picture-card" class="avatar-uploader"
         :show-upload-list="false" action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         :before-upload="beforeUpload" @change="handleChange">
+        <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+        <div v-else>
+            <loading-outlined v-if="loading"></loading-outlined>
+            <plus-outlined v-else></plus-outlined>
+            <div class="ant-upload-text">Upload</div>
+        </div>
+    </a-upload> -->
+    <a-upload v-model:file-list="fileList" name="avatar" list-type="picture-card" class="avatar-uploader"
+        :show-upload-list="false" action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+         @change="handleChange">
         <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
         <div v-else>
             <loading-outlined v-if="loading"></loading-outlined>
@@ -14,13 +24,14 @@
 import { PlusOutlined, LoadingOutlined } from '@ant-design/icons-vue';
 import { message } from 'ant-design-vue';
 import { ref } from 'vue';
-import type { UploadChangeParam, UploadProps } from 'ant-design-vue';
+import type { UploadChangeParam } from 'ant-design-vue';
+// import type { UploadChangeParam, UploadProps } from 'ant-design-vue';
 
-function getBase64(img: Blob, callback: (base64Url: string) => void) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => callback(reader.result as string));
-    reader.readAsDataURL(img);
-}
+// function getBase64(img: Blob, callback: (base64Url: string) => void) {
+//     const reader = new FileReader();
+//     reader.addEventListener('load', () => callback(reader.result as string));
+//     reader.readAsDataURL(img);
+// }
 
 const fileList = ref([]);
 const loading = ref<boolean>(false);
@@ -33,10 +44,10 @@ const handleChange = (info: UploadChangeParam) => {
     }
     if (info.file.status === 'done') {
         // Get this url from response in real world.
-        getBase64(info.file.originFileObj, (base64Url: string) => {
-            imageUrl.value = base64Url;
-            loading.value = false;
-        });
+        // getBase64(info.file.originFileObj, (base64Url: string) => {
+        //     imageUrl.value = base64Url;
+        //     loading.value = false;
+        // });
     }
     if (info.file.status === 'error') {
         loading.value = false;
@@ -44,17 +55,17 @@ const handleChange = (info: UploadChangeParam) => {
     }
 };
 
-const beforeUpload = (file: UploadProps['fileList'][number]) => {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    if (!isJpgOrPng) {
-        message.error('You can only upload JPG file!');
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-        message.error('Image must smaller than 2MB!');
-    }
-    return isJpgOrPng && isLt2M;
-};
+// const beforeUpload = (file: UploadProps['fileList'][number]) => {
+//     const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+//     if (!isJpgOrPng) {
+//         message.error('You can only upload JPG file!');
+//     }
+//     const isLt2M = file.size / 1024 / 1024 < 2;
+//     if (!isLt2M) {
+//         message.error('Image must smaller than 2MB!');
+//     }
+//     return isJpgOrPng && isLt2M;
+// };
 </script>
 <style>
 .avatar-uploader>.ant-upload {
