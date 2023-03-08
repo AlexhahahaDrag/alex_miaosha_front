0c5dd9c62558261f6630c3f90961fa6fc4a0a37a
majf <734663446@qq.com>
2023-03-08 16:23:05 +0800

    添加遗漏

---
+++ src/views/components/fileInfo.ts
@@ -0,0 +1,9 @@
+export interface FileInfo {
+    id: number,
+    fileName: string,
+    fileSize: number,
+    fileType: string,
+    bucketName: string,
+    fileSystem: string,
+    url: string,
+}
