b2557ff1e511e7960d33414bbb2ec1d61d06155f
alexhaha <734663446@qq.com>
2024-01-01 09:25:32 +0800

    添加遗漏文件

---
+++ src/api/finance/orgInfo/orgInfoTs.ts
@@ -0,0 +1,38 @@
+import {
+  getDataOne,
+  postData,
+  putData,
+  deleteData,
+  baseService,
+} from "@/api/common/index";
+
+const baseOrgInfo = "/api/v1//org-info";
+
+const OrgInfoUrl = {
+  page: "/page",
+  url: "",
+};
+
+export function getOrgInfoPage(params: any, pageNo: number | null | undefined, pageSize : number | null| undefined): Promise<any> {
+  let url = baseService.finance + baseOrgInfo + OrgInfoUrl.page + "?pageNum=" + (pageNo ? pageNo : 1) + "&pageSize=" + (pageSize ? pageSize : 10);
+  return postData(url, params);
+}
+
+export function getOrgInfoDetail(id: number): Promise<any> {
+  return getDataOne(baseService.finance + baseOrgInfo + OrgInfoUrl.url + "?id=" + id);
+}
+
+export function deleteOrgInfo(ids: string) : Promise<any>{
+  return deleteData(baseService.finance + baseOrgInfo + OrgInfoUrl.url + "?ids=" + ids);
+}
+
+export function addOrEditOrgInfo(
+  method: string,
+  params: any
+): Promise<any> {
+  if ("put" == method) {
+    return putData(baseService.finance + baseOrgInfo + OrgInfoUrl.url, params);
+  } else {
+    return postData(baseService.finance + baseOrgInfo +  OrgInfoUrl.url, params);
+  }
+}
